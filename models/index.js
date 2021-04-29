ab771802dbb21b6075176a8054faa26c13b98d20
Vivek <vivek.15@mountblue.tech>
2021-04-29 14:51:31 +0530

    Added line db. sequelize and exported

---
+++ models/index.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const config = require('../config');
+const db = {};
+
+let sequelize;
+
+sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: 'mysql',
+
+    pool: {
+        max: 10,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+
+});
+
+
+
+fs
+    .readdirSync(__dirname)
+    .filter(file => {
+        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    })
+    .forEach(file => {
+        const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+        db[model.name] = model;
+    });
+
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+
+module.exports = db;
