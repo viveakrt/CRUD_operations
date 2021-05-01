92cc14e22b13b6cee03804cb57632fe8482898fe
Vivek <vivek.15@mountblue.tech>
2021-05-01 19:12:42 +0530

    Added logger for sequelize

--- test/delete.js
+++ test/delete.js
@@ -7,10 +7,30 @@ chai.should();
 chai.use(chaiHttp);
 
 
-describe('READ DATA', () => {
-    /* 
-    TODO : del.delete('/id/:id'
-    */
+describe('DELETE DATA', () => {
+    it('DELETE using ID', done => {
+        const id = 1
+        chai.request(server)
+        .delete(`/delete/id/${id}`)
+        .end((err,res)=>{
+            
+            if(typeof id != 'number'){
+                res.should.have.status(400);
+            }
+            else{
+                if(res.status==404){
+                    res.body.message.should.equal( `Cosmetic with Id ${id} NOT FOUND`);
+                }else{
+                    res.body.message.should.equal( `Cosmetic with Id ${id} DELETED`);
+                }
+
+            }
+            done();
+
+        })
+    })
+
+
 
     /* 
     TODO: del.delete('/all'
