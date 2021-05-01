106e7865f93a7fcd2a572978ba29087ca519f98b
Vivek <vivek.15@mountblue.tech>
2021-05-01 16:07:01 +0530

    Added GET brands data via name test

--- test/create.js
+++ test/create.js
@@ -7,10 +7,22 @@ chai.should();
 chai.use(chaiHttp);
 
 
-describe('READ DATA', () => {
-    /* 
-    TODO : create.post('/', 
-    */
+describe('POST DATA', () => {
+
+
+    it("POST Data ", done => {
+
+        chai.request(server)
+        .get(`/get/all`)
+        .end((err,res)=>{
+        
+            res.should.have.status(200);
+            res.body.Data.should.be.a('array');
+        
+            done();
+        
+        })
+    })
 
     /* 
     TODO: create.post('/all', 
