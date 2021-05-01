307416e9e27448dea8cdc98897d15c6980a7fbec
Vivek <vivek.15@mountblue.tech>
2021-05-01 12:32:30 +0530

    Added the Create.js and update.js test cases for testing

---
+++ test/create.js
@@ -0,0 +1,18 @@
+const chai = require('chai');
+const chaiHttp = require(`chai-http`);
+const config = require(`../config`);
+let server = require('../server');
+
+chai.should();
+chai.use(chaiHttp);
+
+
+describe('READ DATA', () => {
+    /* 
+    TODO : create.post('/', 
+    */
+
+    /* 
+    TODO: create.post('/all', 
+    */
+})
